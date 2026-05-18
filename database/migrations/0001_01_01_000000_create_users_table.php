5a70b8346b5bf080250f8b0272b761ec57979785
Seb-100 <sebjun95@gmail.com>
2026-05-18 15:46:26 +0100

    seed pb

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -6,17 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->string('tel')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('tel')->unique()->nullable();
+            $table->string('gender')->nullable();
             $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
 
@@ -36,9 +38,6 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('users');
